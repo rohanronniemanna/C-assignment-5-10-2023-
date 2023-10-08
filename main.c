2a6ae5aedc5f5b520266b1103459022d6f36ffc0
rohanronniemanna <144191613+rohanronniemanna@users.noreply.github.com>
2023-10-08 14:21:55 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,31 @@
-
 #include <stdio.h>
-void print_hello_world(int n) {
-    if (n == -1) {
-        return;
+#include <stdlib.h>
+int main()
+{
+    int num1, num2, sum1 = 0, sum2 = 0;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    for (int i = 1; i < num1; i++)
+    {
+        if (num1 % i == 0)
+        {
+            sum1 += i;
+        }
     }
-    printf("Hello, World!\n");
-    print_hello_world( n- 1);
-}
-
-int main() {
-    int n;
-    printf("Enter the number of times to print Hello world : ");
-    scanf("%d", &n);
-    print_hello_world(n);
+    for (int i = 1; i < num2; i++)
+    {
+        if (num2 % i == 0)
+        {
+            sum2 += i;
+        }
+    }
+    if (sum1 == num2 && sum2 == num1)
+    {
+        printf("The two numbers are amicable.");
+    }
+    else
+    {
+        printf("The two numbers are not amicable.");
+    }
     return 0;
 }
