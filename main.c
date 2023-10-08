187a1cf462872cd0d637a8669fef41c1efba9550
rohanronniemanna <144191613+rohanronniemanna@users.noreply.github.com>
2023-10-08 11:43:45 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,32 @@
 
 #include <stdio.h>
-void print_hello_world(int n) {
-    if (n == -1) {
-        return;
+
+void printDiamond(int n) {
+    int space;
+    for (int i = 0; i < n; i++) {
+        for (space = 1; space <= n - i; space++)
+            printf(" ");
+        for (space = 0; space <= 2 * i; space++)
+            printf("* ");
+
+        printf("\n");
     }
-    printf("Hello, World!\n");
-    print_hello_world( n- 1);
+
+    for (int i = n - 1; i > 0; i--) {
+        for (space = 0; space <= n - i; space++)
+            printf(" ");
+
+        for (space = 1; space < 2 * i; space++)
+            printf(" *");
+
+        printf("\n");
+    }
 }
 
 int main() {
     int n;
-    printf("Enter the number of times to print Hello world : ");
+    printf("Enter the number of rows for the diamond pattern: ");
     scanf("%d", &n);
-    print_hello_world(n);
+    printDiamond(n);
     return 0;
 }
